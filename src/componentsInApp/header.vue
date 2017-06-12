390b8f164c781cc7a705243f0e50c7393b266dc8
Shivam Singh <shivampesitbng@gmail.com>
2017-06-12 10:23:25 -0700

    removed partners link from header

--- src/componentsInApp/header.vue
+++ src/componentsInApp/header.vue
@@ -4,7 +4,7 @@
 	<router-link to="/">Logo</router-link>
 	<router-link to="/">home</router-link>
 	<router-link to="/about">about us</router-link>
-	<router-link to="/partners">our partners</router-link>
+	<!--router-link to="/partners">our partners</router-link-->
 	<router-link to="/contact">contact us</router-link>
   </div>
 </template>
